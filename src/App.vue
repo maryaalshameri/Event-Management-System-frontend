5ce16708550820386610c26e65d9ffc626aa5f49
maryaalshameri <marynaalshameri@gmail.com>
2025-09-29 22:33:54 +0300

    feat:Add App

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div >
+    <router-view />
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+
+body {
+  font-family: 'Cairo', sans-serif;
+  direction: rtl;
+}
+</style>
